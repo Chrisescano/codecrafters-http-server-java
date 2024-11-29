dfef4a6bd41ec70c9d22eb600ab1f9c7ad612841
Escano, Christian <escanochristian21@gmail.com>
2024-11-29 16:55:03 -0500

    Stage 3 - Attempt #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,37 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-  public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+    public static void main( String[] args ) {
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        System.out.println( "Logs from your program will appear here!" );
 
-    // Uncomment this block to pass the first stage
+        // Uncomment this block to pass the first stage
 
-     try {
-       ServerSocket serverSocket = new ServerSocket(4221);
+        try {
+            ServerSocket serverSocket = new ServerSocket( 4221 );
+            serverSocket.setReuseAddress( true ); // ensures that we don't run into 'Address already in use' errors
+            Socket socket = serverSocket.accept(); // Wait for connection from client.
+            System.out.println( "accepted new connection" );
 
-       // Since the tester restarts your program quite often, setting SO_REUSEADDR
-       // ensures that we don't run into 'Address already in use' errors
-       serverSocket.setReuseAddress(true);
+            BufferedReader in = new BufferedReader( new InputStreamReader( socket.getInputStream() ) );
+            BufferedWriter out = new BufferedWriter( new OutputStreamWriter( socket.getOutputStream() ) );
 
-       serverSocket.accept(); // Wait for connection from client.
-       System.out.println("accepted new connection");
-     } catch (IOException e) {
-       System.out.println("IOException: " + e.getMessage());
-     }
-  }
+            String message;
+            while ( ( message = in.readLine() ) != null ) {
+                out.write( "HTTP/1.1 200 OK\r\n\r\n" );
+                out.flush();
+            }
+
+            in.close();
+            out.close();
+        } catch ( IOException e ) {
+            System.out.println( "IOException: " + e.getMessage() );
+        }
+    }
 }
