f8818a7c6a531588f0e3f793092bac4817ddaa9a
Escano, Christian <escanochristian21@gmail.com>
2024-11-29 23:45:35 -0500

    Stage 8 - Attempt #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,49 +13,86 @@ public class Main {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
         System.out.println( "Logs from your program will appear here!" );
 
-        // Uncomment this block to pass the first stage
-
         try {
             ServerSocket serverSocket = new ServerSocket( 4221 );
             serverSocket.setReuseAddress( true ); // ensures that we don't run into 'Address already in use' errors
-            Socket socket = serverSocket.accept(); // Wait for connection from client.
-            System.out.println( "accepted new connection" );
-            BufferedReader in = new BufferedReader( new InputStreamReader( socket.getInputStream() ) );
-            BufferedWriter out = new BufferedWriter( new OutputStreamWriter( socket.getOutputStream() ) );
-            StringBuilder request = new StringBuilder();
 
             while ( true ) {
-                String line = in.readLine();
-                request.append( line ).append( HttpConstants.CRLF );
-                if ( line == null || line.isEmpty() ) {
-                    break;
-                }
-            }
+                Socket socket = serverSocket.accept(); // Wait for connection from client.
 
-            System.out.printf( "Received Request: %s\n", request );
-            HttpRequest httpRequest = HttpRequest.parseRequest( request.toString() );
-            if ( httpRequest.getRequestTarget().equals( "/" ) ) {
-                out.write( String.format( "HTTP/1.1 200 OK%s%s", HttpConstants.CRLF, HttpConstants.CRLF ) );
-            } else if ( httpRequest.getRequestTarget().startsWith( "/echo/" )) {
-                String echo = httpRequest.getRequestTarget().substring( 6 );
-                out.write( String.format( "HTTP/1.1 200 OK%sContent-Type: text/plain%sContent-Length: %d%s%s%s",
-                        HttpConstants.CRLF, HttpConstants.CRLF, echo.length(), HttpConstants.CRLF, HttpConstants.CRLF, echo ) );
-            } else if ( httpRequest.getRequestTarget().equals( "/user-agent" ) ) {
-                Map<String, String> headers = httpRequest.getHeaders();
-                String userAgent = headers.get( "User-Agent" );
-                out.write( String.format(
-                        "HTTP/1.1 200 OK%sContent-Type: text/plain%sContent-Length: %d%s%s%s",
-                        HttpConstants.CRLF, HttpConstants.CRLF, userAgent.length(), HttpConstants.CRLF, HttpConstants.CRLF, userAgent
-                ) );
-            } else {
-                out.write( String.format( "HTTP/1.1 404 Not Found%s%s", HttpConstants.CRLF, HttpConstants.CRLF ) );
-            }
+                Thread connectionThread = new Thread(() -> {
+                    System.out.println( "accepted new connection" );
+                    BufferedReader in;
+                    BufferedWriter out;
 
-            out.flush();
-            in.close();
-            out.close();
-            socket.close();
-            serverSocket.close();
+                    try {
+                        in = new BufferedReader( new InputStreamReader( socket.getInputStream() ) );
+                        out = new BufferedWriter( new OutputStreamWriter( socket.getOutputStream() ) );
+                    } catch ( IOException e ) {
+                        throw new RuntimeException( e );
+                    }
+                    StringBuilder request = new StringBuilder();
+
+                    while ( true ) {
+                        String line;
+                        try {
+                            line = in.readLine();
+                        } catch ( IOException e ) {
+                            throw new RuntimeException( e );
+                        }
+
+                        request.append( line ).append( HttpConstants.CRLF );
+                        if ( line == null || line.isEmpty() ) {
+                            break;
+                        }
+                    }
+
+                    System.out.printf( "Received Request: %s\n", request );
+                    HttpRequest httpRequest = HttpRequest.parseRequest( request.toString() );
+                    if ( httpRequest.getRequestTarget().equals( "/" ) ) {
+                        try {
+                            out.write( String.format( "HTTP/1.1 200 OK%s%s", HttpConstants.CRLF, HttpConstants.CRLF ) );
+                        } catch ( IOException e ) {
+                            throw new RuntimeException( e );
+                        }
+                    } else if ( httpRequest.getRequestTarget().startsWith( "/echo/" )) {
+                        String echo = httpRequest.getRequestTarget().substring( 6 );
+                        try {
+                            out.write( String.format( "HTTP/1.1 200 OK%sContent-Type: text/plain%sContent-Length: %d%s%s%s",
+                                    HttpConstants.CRLF, HttpConstants.CRLF, echo.length(), HttpConstants.CRLF, HttpConstants.CRLF, echo ) );
+                        } catch ( IOException e ) {
+                            throw new RuntimeException( e );
+                        }
+                    } else if ( httpRequest.getRequestTarget().equals( "/user-agent" ) ) {
+                        Map<String, String> headers = httpRequest.getHeaders();
+                        String userAgent = headers.get( "User-Agent" );
+                        try {
+                            out.write( String.format(
+                                    "HTTP/1.1 200 OK%sContent-Type: text/plain%sContent-Length: %d%s%s%s",
+                                    HttpConstants.CRLF, HttpConstants.CRLF, userAgent.length(), HttpConstants.CRLF, HttpConstants.CRLF, userAgent
+                            ) );
+                        } catch ( IOException e ) {
+                            throw new RuntimeException( e );
+                        }
+                    } else {
+                        try {
+                            out.write( String.format( "HTTP/1.1 404 Not Found%s%s", HttpConstants.CRLF, HttpConstants.CRLF ) );
+                        } catch ( IOException e ) {
+                            throw new RuntimeException( e );
+                        }
+                    }
+
+                    try {
+                        out.flush();
+                        in.close();
+                        out.close();
+                        socket.close();
+                    } catch ( IOException e ) {
+                        throw new RuntimeException( e );
+                    }
+                });
+                connectionThread.start();
+            }
         } catch ( IOException e ) {
             System.out.println( "IOException: " + e.getMessage() );
         }
