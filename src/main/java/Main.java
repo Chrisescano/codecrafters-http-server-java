d5fb1e869886b27cff2aee424625b3fb10ba6dea
Escano, Christian <escanochristian21@gmail.com>
2024-12-01 17:55:03 -0500

    Stage 9 - Attempt #2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -24,11 +24,11 @@ public class Main {
         System.out.println( "Logs from your program will appear here!" );
         try ( ServerSocket serverSocket = new ServerSocket() ) {
             serverSocket.setReuseAddress( true ); // ensures that we don't run into 'Address already in use' errors
+            Socket socket = serverSocket.accept();
 
             while ( true ) {
                 Thread connection = new Thread(() -> {
                     try (
-                            Socket socket = serverSocket.accept();
                             BufferedReader in = new BufferedReader( new InputStreamReader( socket.getInputStream() ) );
                             BufferedWriter out = new BufferedWriter( new OutputStreamWriter( socket.getOutputStream() ) )
                     ) {
